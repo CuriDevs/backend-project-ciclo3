70a95e933036ffbc66fbd1c622219b60b8441b7c
Edgar Guerrero <edgarguerrero37@gmail.com>
2021-10-14 14:15:31 -0500

    Finalizacion update y delete de mongo

--- controllers/ventas/ventasServices.js
+++ controllers/ventas/ventasServices.js
@@ -32,56 +32,61 @@ class ventasServices {
 		const datos = Object.values(data);
 		const consulta = this.findOne(datos.shift());
 
-		if(consulta){
-    	await conexionBd.collection('ventas').insertOne(data);
+		if (consulta) {
+			await conexionBd.collection('ventas').insertOne(data);
 		}
 		throw boom.notFound('Ya se encuentra la venta');
 	}
 
 	async find() {
 		const conexionBd = getBD();
-        //implementar el codigo paa crar el producto en la BD
-        const resultado = await conexionBd.collection('ventas').find({}).toArray()
+		//implementar el codigo paa crar el producto en la BD
+		const resultado = await conexionBd.collection('ventas').find({}).toArray();
 		return resultado;
 	}
 
 	async findOne(id) {
 		const conexionBd = getBD();
 		//Obtenemos el id
-		const filtrar_id = {_id: ObjectId(id)};
+		const filtrar_id = { _id: ObjectId(id) };
 
 		//Consultamos y almacenamos el dato
 		const resultado = await conexionBd.collection('ventas').findOne(filtrar_id);
 
 		const objeto = Object.values(resultado); //convertimos el dato en un array con sus valores
 		//const venta = this.ventas.find(item => item.id === id);
-		
+
 		if (objeto.findIndex(valor => valor.id === id)) { //verificamos si se encuentra repetido o no
 			return resultado;
 			//return ventas
 		}
-		throw boom.notFound( 'Venta no encontrada' );
+		throw boom.notFound('Venta no encontrada');
 	}
 
 	async update(id, changes) {
+		const conexionBd = getBD();
 		const filtrarProducto = { _id: ObjectId(id) };
-		if (!filtrarProducto) {
-			throw boom.notFound( 'Venta no encontrada' );
+		const venta = await conexionBd.collection('ventas').find(filtrarProducto).toArray();
+		if (venta.length === 0) {
+			throw boom.notFound('Venta no encontrada');
+		} else {
+			const operacion = { $set: changes, };
+			const updated = await conexionBd.collection('ventas').updateOne(filtrarProducto, operacion, { upsert: false, returnOriginal: true });
+			const resultado = await conexionBd.collection('ventas').find(filtrarProducto).toArray();
+			return resultado;
 		}
-		const operacion = { $set: changes, };
-    	const conexionBd = getBD();
-    	const resultado = await conexionBd.collection('ventas').updateOne(filtrarProducto, operacion,
-        { upsert: false, returnOriginal: true });
-		return resultado;
-}
+	}
 
 	async delete(id) {
-		const index = this.ventas.findIndex(item => item.id === id);
-		if (index === -1) {
-			throw boom.notFound( 'Venta no encontrada' );
+		const conexionBd = getBD();
+		const filtrarProducto = { _id: ObjectId(id) };
+		const venta = await conexionBd.collection('ventas').find(filtrarProducto).toArray();
+		if (venta.length === 0) {
+			throw boom.notFound('Venta no encontrada');
+		} else {
+			const remove = await conexionBd.collection('ventas').deleteOne(filtrarProducto);
+			return { id };
 		}
-		this.ventas.splice(index, 1);
-		return { id };
 	}
 }
 
