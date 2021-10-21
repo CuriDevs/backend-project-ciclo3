904527ec1a7f59cc1559c6953c332cddcf24b453
vivian walls <doly8328@gmail.com>
2021-10-20 22:36:16 -0500

    back de usuarios

--- BD/bd.js
+++ BD/bd.js
@@ -19,7 +19,7 @@ const conectarBD = (callback) =>{
             console.error('Error conectando a la base de datos');
             return 'error';
         }
-        conexionBd = db.db('cellphone');
+        conexionBd = db.db('Cellphone');
         console.log('Conexion Exitosa');
         return callback();
     });
