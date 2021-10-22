53a4567b222800f770fe52573b7df04b44afa1c8
Edgar Camilo Guerrero <78990370+Guerrero9606@users.noreply.github.com>
2021-10-22 01:56:31 -0500

    Update users.schema.js
    Eliminacion de ISO en date

--- models/users.schema.js
+++ models/users.schema.js
@@ -6,7 +6,7 @@ const Email = joi.string().email();
 const UrlImage = joi.string().uri();
 const Rol = joi.string().min(5).max(15);
 const State = joi.string().min(3).max(15);
-const date = joi.date().iso();
+const date = joi.date();
 
 export const createUsersSchema = joi.object({
 	UserName: UserName.required(),
