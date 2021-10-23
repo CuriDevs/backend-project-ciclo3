8bb748bd572b2bc3ceaf06ccf4243138afe5cf47
Edgar Guerrero <edgarguerrero37@gmail.com>
2021-10-23 14:37:40 -0500

    Modificacion de longitud en el nombre del usuario

--- models/users.schema.js
+++ models/users.schema.js
@@ -1,7 +1,7 @@
 import joi from 'joi';
 
 const id = joi.string().hex();
-const UserName = joi.string().min(1).max(30);
+const UserName = joi.string().min(1).max(100);
 const Email = joi.string().email();
 const UrlImage = joi.string().uri();
 const Rol = joi.string().min(5).max(15);
